0b2bf0d8ec2b7973605ee2ac5dba919277cd8e0a
mrpinkcat <gatien.anizan@gmail.com>
2023-02-20 23:53:59 +0100

    fix(appWrapper): add borderbox by default on app

--- components/apps/appWrapper.js
+++ components/apps/appWrapper.js
@@ -69,6 +69,7 @@ class appWrapper extends HTMLElement {
           width: 100%;
           height: 100%;
           display: block;
+          box-sizing: border-box;
         }
         :host #close {
           all: unset;
