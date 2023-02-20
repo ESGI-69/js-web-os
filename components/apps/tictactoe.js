9b5f8de6a13643f44aa323fa6730a021e459c687
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-02-20 18:52:52 +0100

    feat(tictactoe-app): refacto all tic tact toe

--- components/apps/tictactoe.js
+++ components/apps/tictactoe.js
@@ -1,431 +1,189 @@
 class appTictactoe extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: 'open'});
-    this.active;
-    this.enabled = false;
-    this.symbols = {
-      'x' : '<i class="fa fa-times txt"></i>',
-      'o' : '<i class="fa fa-circle-o txt"></i>',
-      '?' : '<i class="fa fa-question txt"></i>'
+    this.shadow = this.attachShadow({ mode: 'open' });
+    this.player = 'X';
+    this.computer = 'O';
+    this.board = ['', '', '', '', '', '', '', '', ''];
+    this.scores = {
+      player: 0,
+      computer: 0,
+      draw: 0
     };
-    this.human = false;
-    this.board;
-    this.moves;
-  }
-
-  connectedCallback() {
+    this.gameOver = false;
     this.render();
-    this.load();
-  }
-
-  load() {
-    console.log("load");
-    $('.el-s').click(function(){
-      $('.screen').fadeOut();
-      $('.select').fadeOut();
-      if($(this).attr('id') === 'o'){
-        this.human = false;
-        this.enabled = false;
-        setTimeout(function(){
-          this.enabled = true;
-          $('#n'+this.computerMove()).click();
-        },500);
-      }
-    });
-
-    $('.console').click(this.init).click();
-  }
-    
-  toggleState(act){
-    if (act === 'o')
-      act = 'x';
-    else
-      act = 'o'
-    return act;
-  }
-    
-  checkPatterns(pos){
-    var row = Math.floor(pos/3);
-    var col = pos%3;
-    var diag = (col === row) ? 1 : ((col === 0 || col === 2) && (row === 2 || row === 0)) ? 2 : 0;
-    var base = this.board[pos];
-      if(this.board[row*3] === this.board[row*3 +1 ] && this.board[row*3] === this.board[row*3 + 2])
-        return {pattern : 'r', number : row , symbol : base};
-      if(this.board[col] === this.board[col + 3] && this.board[col] === this.board[col+6])
-        return {pattern : 'c', number : col , symbol : base};
-    if(diag === 1){
-      if(this.board[0] === this.board[4] && this.board[0] === this.board[8])
-        return {pattern : 'd', number : 1 , symbol : base};
-      if(row == 1 && col == 1){
-        if(this.board[2] === this.board[4] && this.board[4] === this.board[6])
-          return {pattern : 'd', number : 2 , symbol : base};
-      }
-    }else if(diag ===2){
-      if(this.board[2] === this.board[4] && this.board[4] === this.board[6])
-        return {pattern : 'd', number : 2 , symbol : base};
-    } 
-    return false;
-  }
-  
-  minimax(symbol,pos,level){
-      var i, res;     
-      var w = this.checkPatterns(pos);
-      if(w){
-        if(w.symbol === this.active)
-          return 100 + level;
-        else
-          return -100 - level;
-      } else if (level === 0 || this.moves === 9) {
-        return 0;
-      }
-      if(symbol !== this.active){
-        res = 1000;
-        for(i=0;i<9;i++){
-        if(this.board[i]==undefined)
-          {
-            this.board[i]=symbol; 
-            this.moves++;
-            res = Math.min(res, this.minimax(this.toggleState(symbol),i,level-1));
-            this.moves--;
-            this.board[i]=undefined;
-          }
-        }
-      }else{
-        res = -1000;
-        for(i=0;i<9;i++){
-        if(this.board[i]==undefined)
-          {
-            this.board[i]=symbol;     
-            this.moves++;
-            res = Math.max(res, this.minimax(this.toggleState(symbol),i,level-1));
-            this.board[i]=undefined;
-            this.moves--;
-          }
-        }
-      }
-      return res;
-  }
-  
-  computerMove(){
-    var i;
-    var max=-1000; 
-    var mi=4;
-    var t;
-        
-    for(i=0;i<9;i++)
-      if(this.board[i]==undefined)
-      {
-        this.board[i]=this.active;
-        this.moves++
-        t=this.minimax(this.toggleState(this.active),i,7);
-        if(t>max)
-        {
-          max=t;
-          mi=i;
-        }
-        this.board[i]=undefined;
-        this.moves--;
-      }
-    return mi;
-  }
-       
-  displayTie(){
-    // TODO
-    $('.out').text("TIE!");
-    $('.el-o').append($(this.symbols['?'])).addClass('tie');
-  }
-    
-  boardClick(){
-    if(this.enabled && this.moves < 9){
-      $(this).append($(this.symbols[this.active])).off('click').removeClass('free');
-      var pos = parseInt($(this).attr('id').slice(1));
-      this.board[pos] = this.active;
-      this.active = this.toggleState(this.active);
-      this.moves++;
-      var w = this.checkPatterns(pos);
-      // game ends
-      if (w){
-        this.displayWin(w);
-        this.enabled = false;
-        setTimeout(function(){
-          $('.screen').fadeIn();
-          $('.console').fadeIn();
-        },500);
-        return;
-      } else if (this.moves === 9){
-        this.enabled = false;
-        this.displayTie();
-        setTimeout(function(){
-          $('.screen').fadeIn();
-          $('.console').fadeIn();
-        },500);
-        return;
-      } else {
-        this.human = !this.human;
-        if(!this.human){
-          this.enabled=false;
-            setTimeout(function(){
-              this.enabled=true;
-              $('#n'+this.computerMove()).click();
-            },500);
-        }
-        return;
-      }
-    }   
-  }
-    
-  displayWin(obj){
-    $('.el').removeClass('free').off('click');
-    switch (obj.pattern) {
-      case 'r' :
-        $('div.r'+obj.number+' div').addClass('win');
-        break;
-      case 'c' :
-        $('.c'+obj.number).addClass('win');
-        break;
-      case 'd' :
-        $('.d'+obj.number).addClass('win');
-        break;       
-    }
-    $('.out').text('WINS!')
-    $('.el-o').append($(this.symbols[obj.symbol])).addClass('win');
-  }
-  
-  init(){
-    console.log("init")
-    $('.el').empty().off('click').click(this.boardClick).removeClass('win').addClass('free');
-    $('.el-o').empty().removeClass('win tie');
-    this.active = 'x';
-    this.board = new Array(9);
-    this.enabled = true;
-    this.human = true;
-    this.moves = 0;
-    $('.screen').fadeIn();
-    $('.select').fadeIn();
-    $('.console').fadeOut();
   }
 
   render() {
     this.shadow.innerHTML = `
       <style>
-        :host html, :host body, :host div, :host span, :host applet, :host object, :host iframe,
-        :host h1, :host h2, :host h3, :host h4, :host h5, :host h6, :host p, :host blockquote, :host pre,
-        :host a, :host abbr, :host acronym, :host address, :host big, :host cite, :host code,
-        :host del, :host dfn, :host em, :host img, :host ins, :host kbd, :host q, :host s, :host samp,
-        :host small, :host strike, :host strong, :host sub, :host sup, :host tt, :host var,
-        :host b, :host u, :host i, :host center,
-        :host dl, :host dt, :host dd, :host ol, :host ul, :host li,
-        :host fieldset, :host form, :host label, :host legend,
-        :host table, :host caption, :host tbody, :host tfoot, :host thead, :host tr, :host th, :host td,
-        :host article, :host aside, :host canvas, :host details, :host embed, 
-        :host figure, :host figcaption, :host footer, :host header, :host hgroup, 
-        :host menu, :host nav, :host output, :host ruby, :host section, :host summary,
-        :host time, :host mark, :host audio, :host video {
-          margin: 0;
-          padding: 0;
-          border: 0;
-          font-size: 100%;
-          font: inherit;
-          vertical-align: middle;
-          
-          -webkit-user-select: none; /* Chrome/Safari */        
-          -moz-user-select: none; /* Firefox */
-          -ms-user-select: none; /* IE10+ */
-        
-          /* Rules below not implemented in browsers yet */
-          -o-user-select: none;
-          user-select: none;
-        }
-        
         :host body {
-          font-family : lato,sans;
+          font-family: Lato, sans-serif;
         }
-        
-        :host div.head h1 {
-          color:#444;
-          font-size : 4em;
-          margin : 30px auto;
-          text-align : center;
+
+        :host .main {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
         }
-        
-        :host .curs {
-          font-family: 'Pacifico',serif;
-        }
-        
-        :host .wrap {
-          position:relative;
-          margin:auto;
-          margin-top: 40px;
-          width : 400px;
-          
-        }
-        
-        :host .row {
-          text-align:center;
-          margin : 10px auto;
-        }
-        
-        :host div.pop-up h3 {
-          text-align:center;
-          padding-top: 5px;
-          color: #444;
-        }
-        
-        :host .inline {
-          display:inline-block;
-          margin:10px 5px;
-          text-align:center;  
-        }
-        
-        :host .big {
-          font-size:2em;
-          margin : 30px auto;
-        }
-        
-        :host #sub {
-          color: #888;
-        }
-        
-        :host .el,.el-s,.el-o {
-          display: inline-block;
-          height:110px;
-          width:110px;
-          font-size : 4.5em;
-          background-color : #61b2a3;
-          border-radius:20px;
-          margin: auto 5px;
-          color: #f8f8f8;
-        }
-        :host .el i,.el-s i,.el-o i {
-          padding-bottom: 8px;
-        }
-        :host .el-o {
-          margin-right: 20px;
-        }
-        :host .screen {
-          display:none;
-          background-color:rgba(10,10,10,0.6);
-          position:fixed;
-          width: 100%;
-          height : 100%;
-          top : 0;
-        }
-        
-        :host .pop-up {
-          position:absolute;
-          width : 400px;
-          height : 175px;
-          border-radius: 20px;
-          background-color:#fff;
+
+        :host .board {
+          // display: flex;
+          // flex-wrap: wrap;
+          // justify-content: center;
+          // margin-top: 20px;
+          width: 600px;
+          height: 600px;
           margin: 0 auto;
-          top  : 250px;
-          left : 50%;
-          margin-left: -200px;
-          box-shadow: 0px 5px 5px -2px rgba(20,20,20, 0.4);
-          display:none;
-        }
-        
-        :host .win {
-          background-color: #E6382B;
-        }
-        
-        :host .tie {
-          background-color: #a5bcae;
-        }
-        
-        :host .free {
-          cursor : pointer;
-          background-color: #71A299;
-        }
-        
-        :host .free:hover {
-          background-color : #61b2a3;
-        }
-        
-        /* Scaling for Mobile */
-        @media screen and (max-width: 450px){
-        
-          :host .head {
-            display:none;
-          }
-          :host .wrap {
-            top: 30px;
-            
-            -moz-transform: scale(0.7);
-            -webkit-transform: scale(0.7);
-            transform: scale(0.7);
-            
-            -webkit-transform-origin: 0 0;
-            -moz-transform-origin: 0 0;
-            transform-origin: 0 0;
-            
-            left: 50%;
-            margin-left: -140px;
-          }
+          background-color: #34495e;
+          color: #fff;
+          border: 6px solid #2c3e50;
+          border-radius: 10px;
           
-          :host .pop-up {
-            
-            top: 100px;
-            -moz-transform: scale(0.7);
-            -webkit-transform: scale(0.7);
-            transform: scale(0.7);
-            
-            -webkit-transform-origin: 0 0;
-            -moz-transform-origin: 0 0;
-            transform-origin: 0 0;
-            
-            left: 50%;
-            margin-left: -140px;
-          }
+          display: grid;
+          grid-template: repeat(3, 1fr) / repeat(3, 1fr);
         }
-        
-        :host .txt {
-          position: relative;
-          top: 5px;
+
+        :host .board div {
+          // width: 60px;
+          // height: 60px;
+          // border: 1px solid black;
+          // display: flex;
+          // align-items: center;
+          // justify-content: center;
+          // font-size: 2rem;
+          // cursor: pointer;
+          border: 6px solid #2c3e50;
+          border-radius: 2px;
+          font-family: Helvetica;
+          font-weight: bold;
+          font-size: 4em;
+          display: flex;
+          justify-content: center;
+          align-items: center;
         }
+
+        :host .scores {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 20px;
+        }
+
+        :host .reset {
+          margin-top: 20px;
+          cursor: pointer;
+        }
+
       </style>
 
-      <div class="head"><h1 class="curs">Tic Tac Toe</h1></div>
-      <div class="wrap">
-        <div class="row r0">
-          <div id='n0' class="el c0 d1"></div>
-          <div id='n1' class="el c1"></div>
-          <div id='n2' class="el c2 d2"></div>
+      <div class="main">
+        <div class="choose-symbol">
+          <p>Choose your symbol:</p>
+          <button id="x-button">X</button>
+          <button id="o-button">O</button>
         </div>
-        <div class="row r1">
-          <div id='n3' class="el c0"></div>
-          <div id='n4' class="el c1 d1 d2"></div>
-          <div id='n5' class="el c2"></div>
+        <div class="board"></div>
+        <div class="scores">
+          <p>Player: ${this.scores.player}</p>
+          <p>Computer: ${this.scores.computer}</p>
+          <p>Draw: ${this.scores.draw}</p>
         </div>
-        <div class="row r2">
-          <div id='n6' class="el c0 d2"></div>
-          <div id='n7' class="el c1"></div>
-          <div id='n8' class="el c2 d1"></div>
-        </div>
-      </div>
-
-      <div class="screen"></div>
-        
-      <div class="console pop-up">
-        <div class="row">
-          <div class="el-o"></i></div>
-          <h3 class="big inline out">WINS!</h3>
-        </div>
-        <h3 id="sub">click inside to play again</h3>
-      </div>
-
-      <div class="select pop-up">
-        <h3>PLEASE SELECT:</h3>
-        <div class="row">
-          <div id='x' class="el-s free"><i class="fa fa-times txt"></i></div>
-          <h3 class='inline'>OR</h3>
-          <div id ='o' class="el-s free"><i class="fa fa-circle-o txt"></i></div>
-        </div>
+        <div class="reset">Reset</div>
       </div>
     `;
+    this.boardDiv = this.shadow.querySelector('.board');
+    this.resetDiv = this.shadow.querySelector('.reset');
+    this.xButton = this.shadow.querySelector('#x-button');
+    this.oButton = this.shadow.querySelector('#o-button');
+    this.xButton.addEventListener('click', () => {
+      this.player = 'X';
+      this.computer = 'O';
+      this.startNewGame();
+    });
+    this.oButton.addEventListener('click', () => {
+      this.player = 'O';
+      this.computer = 'X';
+      this.startNewGame();
+    });
+    this.resetDiv.addEventListener('click', () => {
+      this.startNewGame();
+    });
   }
 
+  startNewGame() {
+    this.board = ['', '', '', '', '', '', '', '', ''];
+    this.gameOver = false;
+    this.renderBoard();
+  }
+
+  renderBoard() {
+    let html = '';
+    for (let i = 0; i < this.board.length; i++) {
+      html += `<div data-index="${i}">${this.board[i]}</div>`;
+      if ((i + 1) % 3 === 0) {
+        html;
+      }
+    }
+    this.boardDiv.innerHTML = html;
+    const boardDivs = this.shadow.querySelectorAll('.board div');
+    boardDivs.forEach((div) => {
+      div.addEventListener('click', () => {
+        if (div.innerText === '' && !this.gameOver) {
+          this.makeMove(div.dataset.index, this.player);
+          if (!this.gameOver) {
+            this.computerMove();
+          }
+        }
+      });
+    });
+  }
+  makeMove(index, symbol) {
+    this.board[index] = symbol;
+    this.renderBoard();
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < winningLines.length; i++) {
+      const [a, b, c] = winningLines[i];
+      if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+        this.gameOver = true;
+        if (this.board[a] === this.player) {
+          this.scores.player++;
+          alert('You win!');
+        } else {
+          this.scores.computer++;
+          alert('You lose!');
+        }
+        this.render();
+        return;
+      }
+    }
+    if (this.board.filter((x) => x === '').length === 0) {
+      this.gameOver = true;
+      this.scores.draw++;
+      alert('Draw!');
+      this.render();
+    }
+  }
+
+  computerMove() {
+    const emptyIndices = [];
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[i] === '') {
+        emptyIndices.push(i);
+      }
+    }
+    const randomIndex = emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
+    this.makeMove(randomIndex, this.computer);
+  }
 }
 
 customElements.define('os-app-tictactoe', appTictactoe);
