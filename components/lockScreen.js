61a63e2533b896394da22db144d1df45678e2857
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-03-01 01:15:46 +0100

    feat(lock-screen): done without slide

--- components/lockScreen.js
+++ components/lockScreen.js
@@ -7,36 +7,76 @@ class LockScreen extends HTMLElement {
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: 'open' });
+    this.password = getSettingValue(findSetting('lockscreen-password'));
   }
 
   connectedCallback() {
     this.render();
+    this.getLockScreen();
+  }
 
+  getLockScreen() {
+    const method = getSettingValue(findSetting('lockscreen-method'));
+    if (method === 'none') {
+      this.sliderLockScreen();
+    } else if (method === 'password') {
+      this.passwordLockScreen();
+    }
+  }
+
+  sliderLockScreen() {
     this.unlockButton = this.shadow.querySelector('#slider');
     this.unlockButton.addEventListener('click', () => {
       this.unlock();
-    });
-
+    }); 
   }
 
+  passwordLockScreen() {
+    this.passwordInput = this.shadow.querySelector('#password');
+    this.submitButton = this.shadow.querySelector('#submit-password');
+  
+    const submitHandler = () => {
+      if (this.passwordInput.value === this.password) {
+        console.log("password correct");
+        this.unlock();
+      } else {
+        console.log("password incorrect");
+        this.shadow.querySelector('#wrong-password').style.display = "block";
+      }
+    };
+  
+    this.submitButton.addEventListener('click', submitHandler);
+  
+    this.passwordInput.addEventListener('keydown', (event) => {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        submitHandler();
+      }
+    });
+  }
+
+  
   unlock() {
     this.remove();
-  }
-
+  } 
+ 
   generateHtmlWithSettings() {
     const method = getSettingValue(findSetting('lockscreen-method'));
     if (method === 'none') {
       return `
-      <div id="container">
-        <div id="slider">
-          <img id="unlock" src="./assets/images/app-icons/unlock.svg">
+        <div id="container">
+          <div id="slider">
+            <img id="unlock" src="./assets/images/app-icons/unlock.svg">
+          </div>
+          <p>Slide to unlock</p>
         </div>
-        <p>Slide to unlock</p>
-      </div>
       `;
     } else if (method === 'password') {
       return `
-      <div id="slider">test<img id="unlock" src="./assets/images/app-icons/unlock.svg"></div>
+        <div id="container-password">
+          <input type="password" id="password" placeholder="Password">
+          <button id="submit-password">Unlock</button>
+        </div>
       `;
     }
   }
@@ -72,7 +112,42 @@ class LockScreen extends HTMLElement {
           user-select: none;  
         }
 
+        :host #container-password {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 50%;
+          flex-direction: column;
+        }
+
+        :host #container-password #password {
+          width: 200px;
+          height: 30px;
+          padding: 16px;
+          background-color: transparent;
+          border: 1px solid var(--color-dock-background);
+          border-radius: 16px;
+          color: white;
+          font-size: 18px;
+        }
+
+        :host #container-password #password::placeholder {
+          color: var(--color-dock-background);
+        }
+        
+
+        :host #submit-password {
+          margin-top: 3rem;
+          border: 1px solid var(--color-dock-background);
+          border-radius: 16px;
+          color: var(--color-text);
+          font-size: 18px;
+          padding: 16px 32px;
+          background-color: transparent;
+        }
+
         :host #container {
+          top: 65%;
           width:270px;
           height:65px;
           background: var(--color-dock-background);
@@ -126,6 +201,7 @@ class LockScreen extends HTMLElement {
 
       </style>
       ${this.generateHtmlWithSettings()}
+      <p id="wrong-password" style="display: none;"> Wrong password </p>
     `;
   }
 }
